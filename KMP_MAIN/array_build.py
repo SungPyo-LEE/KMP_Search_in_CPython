3fb800551e6ca4a71da31995d7eab8471efce895
SAXYCOW <53338488+SungPyo-LEE@users.noreply.github.com>
2020-06-01 23:15:10 +0900

    KMP 메인... 잘 되는 것 같긴 한데 비어있는 배열에서 자꾸 주소값을 내뱉는다

---
+++ KMP_MAIN/array_build.py
@@ -0,0 +1,10 @@
+  
+from distutils.core import setup, Extension
+import numpy
+
+# define the extension module
+kmp_module_np = Extension('kmp_search', sources=['kmp_array.c'],
+                          include_dirs=[numpy.get_include()])
+
+# run the setup
+setup(ext_modules=[kmp_module_np])
---
+++ KMP_MAIN/kmp_array.c
@@ -0,0 +1,123 @@
+#include <Python.h>
+#include <numpy/arrayobject.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_BUFFER 512
+int Border[MAX_BUFFER];
+
+void Preprocess(char* Pattern, int PatternSize)
+{
+	int i = 0;
+	int j = -1;
+	
+	Border[0] = -1;
+	while (i < PatternSize)
+	{
+		while(j>-1 && Pattern[i] != Pattern[j])
+            j = Border[j];
+        i++;
+        j++;
+
+        Border[i] = j;
+    }
+}
+
+double KMP(char* Text, int TextSize, int Start, char* Pattern, int PatternSize)
+{
+    int i = Start;
+    int j = 0;
+    double Position = -1;
+    Preprocess(Pattern, PatternSize);
+
+    while(i < TextSize)
+    {
+        while(j>=0 && Text[i] != Pattern[j])
+            j = Border[j];
+        i++;
+        j++;
+        if (j == PatternSize)
+        {
+            Position = (double)i - (double)j;
+            break;
+        }
+    }
+    return Position;
+}
+
+double *pyvector_to_Carrayptrs(PyArrayObject *arrayin);
+
+static PyObject* kmp_search(PyObject *self, PyObject *args) 
+{
+    const char* text;
+    const char* pattern;
+    if (!PyArg_ParseTuple(args, "ss", &text, &pattern)) {
+        return NULL;
+    }
+    int P_size = strlen(pattern);
+    int n = strlen(text);
+    double Position = KMP(text, strlen(text), 0, pattern, P_size);
+    int i = 0;
+    int cnt = 0;
+    double *vector={NULL,};
+    npy_intp dims[1] = {20};
+    npy_intp k;
+
+    PyObject *ret = PyArray_SimpleNew(1, dims, NPY_DOUBLE);
+    vector = (double *) PyArray_DATA(ret);
+    /*
+     *  NOTE: Treating PyArray_DATA(ret) as if it were a contiguous one-dimensional C
+     *  array is safe, because we just created it with PyArray_SimpleNew, so we know
+     *  that it is, in fact, a one-dimensional contiguous array.
+     */
+    while (i < n)
+    {
+        double Position = KMP(text, strlen(text), i, pattern, P_size);
+        printf("%lf, %s \n", Position +1, text);
+        i = Position + P_size;
+		if (vector[cnt-1]>=Position + 1) break;
+		else
+		{
+			vector[cnt] = Position + 1;
+		}
+        cnt += 1;
+        
+    }
+    return ret;
+}
+
+double *pyvector_to_Carrayptrs(PyArrayObject *arrayin)
+{
+  int n=arrayin->dimensions[0];
+  return (double *) arrayin->data;  /* pointer to arrayin data as double */
+}
+
+static PyMethodDef kmp_array_methods[] = { 
+	{"kmp_search", kmp_search, METH_VARARGS},
+		{NULL, NULL, 0}
+};
+
+
+static struct PyModuleDef kmp_search_mods = {
+
+		PyModuleDef_HEAD_INIT,
+
+		"kmp_search",					// 모듈의 이름
+
+		"It is test module.",			// 모듈의 설명을 적는 부분, 모듈의 __doc__에 저장됨
+
+		-1, kmp_array_methods
+
+};
+
+PyMODINIT_FUNC PyInit_kmp_search(void)
+{
+    PyObject *module;
+    module = PyModule_Create(&kmp_search_mods);
+    if(module==NULL) return NULL;
+    /* IMPORTANT: this must be called */
+    import_array();
+    if (PyErr_Occurred()) return NULL;
+    return module;
+}
