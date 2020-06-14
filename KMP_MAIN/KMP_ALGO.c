1edb264a85ef09255112215af03a3b263ceeed5f
SAXYCOW <53338488+SungPyo-LEE@users.noreply.github.com>
2020-06-14 19:15:13 +0900

    KMP 동적 배열

--- KMP_MAIN/KMP_ALGO.c
+++ KMP_MAIN/KMP_ALGO.c
@@ -2,67 +2,87 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int knuthMorrisPratt(char* text, int textSize, int start, char* pattern, int patternSize);
+#define MAX_BUFFER 512
+int Border[MAX_BUFFER];
 
-void preprocess(char* pattern, int patternSize, int* border);
-
-int knuthMorrisPratt(char* text, int textSize, int start, char* pattern, int patternSize)
-{
-	int i = start;
-	int j = 0;
-	int position = -1;
-	int* border = (int*)calloc(patternSize+1, sizeof(int));
-	preprocess(pattern, patternSize, border);
-	while( i < textSize )
-	{
-		while( j >= 0 && text[i] != pattern[j] )
-			j = border[j];
-		i++;
-		j++;
-		if( j == patternSize )
-		{
-		position = i - j;
-		break;
-		}
-	}
-	free(border);
-	return position;
-}
-
-void preprocess(char* pattern, int patternSize, int* border)
+void Preprocess(char* Pattern, int PatternSize)
 {
 	int i = 0;
 	int j = -1;
-	border[0] = -1;
-	while( i < patternSize )
+	
+	Border[0] = -1;
+	while (i < PatternSize)
 	{
-		while( j > -1 && pattern[i] != pattern[j] )
-			j = border[j];
-		i++;
-		j++;
-	border[i] = j;
-	}
+		while(j>-1 && Pattern[i] != Pattern[j])
+            j = Border[j];
+        i++;
+        j++;
+
+        Border[i] = j;
+    }
+}
+
+double KMP(char* Text, int TextSize, int Start, char* Pattern, int PatternSize)
+{
+    int i = Start;
+    int j = 0;
+    double Position = -1;
+    Preprocess(Pattern, PatternSize);
+
+    while(i < TextSize)
+    {
+        while(j>=0 && Text[i] != Pattern[j])
+            j = Border[j];
+        i++;
+        j++;
+        if (j == PatternSize)
+        {
+            Position = (double)i - (double)j;
+            break;
+        }
+    }
+    return Position;
 }
 
 int main()
 {
-    char Text[] = "ABC ABCDAB ABCDABCDABDE";
-    char Pattern[] = "AB";
+	double *vector;
+    char Text[] = "ABC ABCDAB";
+    char Pattern[] = "ABC";
     int P_size = strlen(Pattern);
     int n = strlen(Text);
-    double Position = knuthMorrisPratt(Text, strlen(Text), 0, Pattern, P_size);
-	printf("%f\n", Position+1);
-    int i_1 = Position + 1;
-	int first = i_1;
-	Position =knuthMorrisPratt(Text, strlen(Text), i_1, Pattern, P_size);
-	int i_2 = Position+1;
-	printf("%d\n",first);
-	i_1 = Position + 1;
-	while (i_2 > first)
-	{
-		Position =knuthMorrisPratt(Text, strlen(Text), i_2+1, Pattern, P_size);
-		int i_2 = Position+1;
-		printf("%f\n", Position+1);
-	}
-	return 0;
+    double Position = KMP(Text, strlen(Text), 0, Pattern, P_size);
+    int i = 0;
+	int cnt = 0;
+    while (i < n)
+    {
+        double Position = KMP(Text, strlen(Text), i, Pattern, P_size);
+        printf("%lf, %s \n", Position, Text);
+		if (Position + P_size > i || i == 0)
+		{
+			i = Position + P_size;
+			cnt += 1;
+		}
+		else
+			break;
+    }
+	vector = (double*)malloc(sizeof(double) * cnt - 1);
+	i = 0;
+    while (i < n)
+    {
+        double Position = KMP(Text, strlen(Text), i, Pattern, P_size);
+        //printf("%lf, %s \n", Position, Text);
+		if (Position + P_size > i || i == 0)
+		{
+			i = Position + P_size;
+			vector[i]=Position;
+		}
+		else
+			break;
+    }
+	int j = sizeof(vector)/sizeof(double);
+	for (int i =0; i<j; ++i)
+		printf("%lf", vector[i]);
+	free(vector);
+    return 0;
 }
