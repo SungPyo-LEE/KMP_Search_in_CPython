12bf24167807a925cea3a33d779789b3fdbc9442
sung-pyo LEE <kh2mail@naver.com>
2020-06-20 08:22:44 +0900

    kmp_search by C in python

--- KMP_MAIN/kmp_header.h
+++
@@ -1,8 +0,0 @@
-#include <Python.h>
-#include <numpy/arrayobject.h>
-#include <math.h>
-#include <string.h>
-#include <stdio.h>
-
-void Preprocess(char* Pattern, int PatternSize);
-double KMP(char* Text, int TextSize, int Start, char* Pattern, int PatternSize);
